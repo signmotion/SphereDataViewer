33bbc7a9d8b731f3f30c9b97f9271f830c26bfd7
Andrey Syrokomskiy <signmotion@gmail.com>
2019-06-26 23:13:40 +0300

    Extended Vec3.

--- Test/FrameBuffer.cpp
+++ Test/FrameBuffer.cpp
@@ -4,29 +4,73 @@
 #include <algorithm>
 
 
-struct Vec3
+class Vec3
 {
-	float x, y, z;
+public:
+	Vec3() :
+		Vec3(0.f, 0.f, 0.f)
+	{}
 
-	Vec3 normalize() const
+	Vec3(const Vec3& b) :
+		Vec3(b.x(), b.y(), b.z())
+	{}
+
+	Vec3(float x, float y, float z) :
+		m_x(x), m_y(y), m_z(z)
+	{}
+
+	Vec3& operator=(const Vec3& b) {
+		m_x = b.m_x;
+		m_y = b.m_y;
+		m_z = b.m_z;
+		return *this;
+	}
+
+	float x() const {
+		return m_x;
+	}
+
+	float y() const {
+		return m_y;
+	}
+
+	float z() const {
+		return m_z;
+	}
+
+	Vec3 normalizeCopy() const
 	{
-		const float d = 1.f / sqrt(x * x + y * y + z * z);
-		Vec3 vec;
-		vec.x = x * d;
-		vec.y = y * d;
-		vec.z = z * d;
-		return vec;
+		const float d = 1.f / length();
+		return Vec3{ m_x * d, m_y * d, m_z * d };
+	}
+
+	void normalize()
+	{
+		const float d = 1.f / length();
+		m_x *= d;
+		m_y *= d;
+		m_z *= d;
+	}
+
+	float length() const
+	{
+		return sqrtf(m_x * m_x + m_y * m_y + m_z * m_z);
 	}
 
 	float dot(const Vec3& v2) const
 	{
-		return x * v2.x + y * v2.y + z * v2.z;
+		return m_x * v2.m_x + m_y * v2.m_y + m_z * v2.m_z;
 	}
+
+private:
+	float m_x;
+	float m_y;
+	float m_z;
 };
 
 
 // Global light
-Vec3 Light;
+Vec3 Light = { 1.f, -0.5f, 0.7f };
 
 
 //////////////////////////////////////////////////////////////////////////
@@ -35,10 +79,7 @@ CFrameBuffer::CFrameBuffer(const int iWidth, const int iHeight)
 {
 	m_FramebufferArray.resize(iWidth * iHeight, 0);
 
-	Light.x = 1.f;
-	Light.y = -0.5f;
-	Light.z = 0.7f;
-	Light = Light.normalize();
+	Light.normalize();
 }
 
 
@@ -95,20 +136,20 @@ void CFrameBuffer::RenderSphere(
 
 			// Phong shading
 			{
-				Vec3 vec_normal;
-				vec_normal.x = (float)dx;
-				vec_normal.y = (float)dy;
-				vec_normal.z = sqrt(radius2 - (dx2 + dy2));
-				vec_normal = vec_normal.normalize();
+				Vec3 vec_normal = {
+					(float)dx,
+					(float)dy,
+					sqrtf(radius2 - (dx2 + dy2)) };
+				vec_normal.normalize();
 
 				const float NdotL = Light.dot(vec_normal);
 				if (NdotL > 0)
 				{
-					Vec3 vec_eye;
-					vec_eye.x = Light.x + fScreenX;
-					vec_eye.y = Light.y + fScreenY;
-					vec_eye.z = Light.z + 1.f;
-					Vec3 vec_half = vec_eye.normalize();
+					const Vec3 vec_eye = {
+						Light.x() + fScreenX,
+						Light.y() + fScreenY,
+						Light.z() + 1.f };
+					const Vec3 vec_half = vec_eye.normalizeCopy();
 
 					const float NdotHV = vec_half.dot(vec_normal);
 					static constexpr float shininess = 12;
